84e823d3e809cf290e0760c2c69e3d62968efe6c
Jessica Benson <pokeyjess72@gmail.com>
2020-11-02 20:10:49 -0500

    Updates

---
+++ precache-manifest.e009d0651dca8aeb6a1c57ef633fe352.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b92be365b865a67fd23e88b2597e770c",
+    "url": "/ToDo-List/index.html"
+  },
+  {
+    "revision": "3d7068ad7f4bd40c9ff6",
+    "url": "/ToDo-List/static/css/main.07455b9b.chunk.css"
+  },
+  {
+    "revision": "9720fb89b91ab37628c0",
+    "url": "/ToDo-List/static/js/2.ececd876.chunk.js"
+  },
+  {
+    "revision": "3d7068ad7f4bd40c9ff6",
+    "url": "/ToDo-List/static/js/main.de8bf671.chunk.js"
+  },
+  {
+    "revision": "8d3febef82a3856f9a28",
+    "url": "/ToDo-List/static/js/runtime-main.3f8aa41c.js"
+  }
+]);
